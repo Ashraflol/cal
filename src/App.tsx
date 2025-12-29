30a719d2cb5bacb7c84980b76af552066ec65cd0
Ashraf Maniyar <a.maniyar6@gmail.com>
2025-12-30 00:23:39 +0530

    added styles and logic complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import './App.css';
 import Calender from './components/Calender';
 
 function App() {
+  const calenderDate = '2025-12-30';
   return (
     <div className="App">
-      <Calender date={'2024-06-15'} />
+      <h1>Calendar Component {calenderDate}</h1>
+      <Calender date={calenderDate} />
     </div>
   );
 }
--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,14 +1,50 @@
+import { useState } from 'react';
 import './calender.css';
 
 function Calender(date: { date: string }) {
-  return (
-    <div className="container">
-      <h1>Calendar Component {date.date}</h1>
-      <div className="calendar">
-        <h6 className='month-year'>June 2024</h6>
-        <div className="weekdays">
 
-        </div>
+  const EpocDate = new Date(date.date);
+  const monthNumber = EpocDate.getMonth() + 1;
+  const yearNumber = EpocDate.getFullYear();
+  const firstDay = new Date(yearNumber, monthNumber - 1, 1);
+  const daysInMonth = new Date(yearNumber, monthNumber, 0).getDate();
+  const day = EpocDate.getDate();
+  const monthname = EpocDate.toLocaleString('default', { month: 'short' });
+
+
+  const listItems = [];
+  for (let i = 0; i < firstDay.getDay(); i++) {
+    listItems.push('');
+  }
+  for (let i = 1; i <= daysInMonth; i++) {
+    listItems.push(i);
+  }
+
+  if (monthNumber < 1 || monthNumber > 12 || day < 1 || day > daysInMonth) {
+    return (
+      <div className="calendar">
+        <h6 className='month-year'>Invalid Date</h6>
+      </div>
+    );
+  }
+
+  return (
+    <div className="calendar">
+      <h6 className='month-year'>{monthname} {yearNumber}</h6>
+      <div className="weekdays-row">
+        <span>Sun</span>
+        <span>Mon</span>
+        <span>Tue</span>
+        <span>Wed</span>
+        <span>Thu</span>
+        <span>Fri</span>
+        <span>Sat</span>
+        {listItems.map((item, index) =>
+        (
+          <span key={index} className={item === day ? "date-cell current-day" : "date-cell"}>
+            {item}
+          </span>
+        ))}
       </div>
     </div>
   );
